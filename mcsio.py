e27b1c8d9883ebcf251aad1a37e02a6220c30c6c
hsiehyingfeng <hsiehyingfeng@gmail.com>
2018-12-03 07:00:01 +0000

    -a

--- mcsio.py
+++ mcsio.py
@@ -22,11 +22,13 @@
 import sys
 import time
 import Adafruit_DHT
-
 import httplib 
 import json
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 deviceId = "DF9HT1vk"
-deviceKey = "re7hEhi0N4h77mNs" 
+deviceKey = "re7hEhi0N4h77mNs"  
 def post_to_mcs(payload): 
 	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
 	not_connected = 1 
@@ -37,13 +39,13 @@ def post_to_mcs(payload):
 			not_connected = 0 
 		except (httplib.client.HTTPException, socket.error) as ex: 
 			print ("Error: %s") % ex 
-			time.sleep(10)
+			time.sleep(1)
 			 # sleep 10 seconds 
 	httpClient.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
 	response = httpClient.getresponse() 
 	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
 	data = response.read() 
-	httpClient.close() 
+	httpClient.close()   
 
 
 
@@ -71,12 +73,19 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while True:
+	s0= GPIO.input(24)
+        if(s0 == 0):
+                print('Button pressed')
+        else:
+                print('Button released')
 	h0, t0 = Adafruit_DHT.read_retry(sensor,4)
 	if h0 is not None and t0 is not None:
 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},{"dataChnId":"Temperature","values":{"value":t0}}]} 
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+			{"dataChnId":"Temperature","values":{"value":t0}},
+			 {"dataChnId":"SwitchStatus","values":{"value":s0}}]} 
 		post_to_mcs(payload)
-		time.sleep(10) 
+		time.sleep(1) 
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
